ef827145c3f152be19c2952674c5656dc81e2fba
jrodriguez0423 <111903824+jrodriguez0423@users.noreply.github.com>
2024-05-18 14:45:20 -0700

    portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,21 @@ import './Footer.css'; // Ensure this file exists and includes necessary styles
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container d-flex justify-content-center">
-        <a href="https://www.linkedin.com/in/justin-rodriguez-575875252/" target="_blank" rel="noopener noreferrer" className="social-icon">
-          <i className="fab fa-linkedin"></i>
-        </a>
-        <a href="https://www.instagram.com/justinprodri/" target="_blank" rel="noopener noreferrer" className="social-icon">
-          <i className="fab fa-instagram"></i>
-        </a>
-        <a href="https://github.com/jrodriguez0423/" target="_blank" rel="noopener noreferrer" className="social-icon">
-          <i className="fab fa-github"></i>
-        </a>
-      </div>
-      <div className="container d-flex justify-content-center">
-        <p className="copyright-text">© 2024 Justin Rodriguez. All Rights Reserved.</p>
+      <div className="container footer-content">
+        <div className="social-icons">
+          <a href="https://www.linkedin.com/in/justin-rodriguez-575875252/" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="https://www.instagram.com/justinprodri/" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://github.com/jrodriguez0423/" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+        <div className="copyright">
+          <p className="copyright-text">© 2024 Justin Rodriguez. All Rights Reserved.</p>
+        </div>
       </div>
     </footer>
   );
